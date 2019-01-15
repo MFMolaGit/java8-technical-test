07ba7f11167f0c390286fc94ee0a1d843b1363d3
Manuel Francisco Mola <manuelfrancisco.mola@bbva.com>
2019-01-15 23:59:48 +0100

    TDD. Validator and Generator tests

--- technical-test-impl/src/test/java/com/ciklum/utilities/TokenGeneratorTest.java
+++ technical-test-impl/src/test/java/com/ciklum/utilities/TokenGeneratorTest.java
@@ -1,11 +1,19 @@
 package com.ciklum.utilities;
 
 import com.ciklum.dtos.User;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class TokenGeneratorTest {
@@ -21,8 +29,17 @@ public class TokenGeneratorTest {
     }
 
     @Test
-    public void generate() {
+    public void testGenerateToken() {
+        //Given
         final User user = podamFactory.manufacturePojo(User.class);
         final String generatedToken = tokenGenerator.generate(user);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+        String formatDateTime = formatter.format(new Date());
+
+        //When
+        String expectedToken = new StringBuilder(user.getUserId()).append("_").append(formatDateTime).toString();
+
+        //Then
+        Assert.assertThat(generatedToken, is(expectedToken));
     }
 }
--- technical-test-impl/src/test/java/com/ciklum/validators/CredentialsValidatorTest.java
+++ technical-test-impl/src/test/java/com/ciklum/validators/CredentialsValidatorTest.java
@@ -1,13 +1,41 @@
 package com.ciklum.validators;
 
+import com.ciklum.dtos.Credentials;
+import com.ciklum.dtos.User;
+import org.junit.Assert;
 import org.junit.Test;
 
+/*
+ * If the password matches the username in uppercase, the validation is a success, otherwise is a failure. Examples:
+ */
 public class CredentialsValidatorTest {
 
+    private CredentialsValidator credentialsValidator = new CredentialsValidator();
+
+    // username: house , password: HOUSE => Valid credentials.
     @Test
-    public void validate() {
+    public void testValidateCredentials() {
+        //Given
+        Credentials credentials = new Credentials.CredentialsBuilder().withUsername("house")
+                .withPassword("HOUSE").build();
+        //When
+        final boolean validation = credentialsValidator.validate(credentials);
 
-
-
+        //Then
+        Assert.assertTrue(validation);
     }
+
+    // username: house , password: House => Invalid credentials.
+    @Test
+    public void testInvalidateCredentials() {
+        //Given
+        Credentials credentials = new Credentials.CredentialsBuilder().withUsername("house")
+                .withPassword("House").build();
+        //When
+        final boolean validation = credentialsValidator.validate(credentials);
+
+        //Then
+        Assert.assertFalse(validation);
+    }
+
 }
